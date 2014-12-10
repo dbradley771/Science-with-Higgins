4bc4cc332004e0ba456c694d1e4c2beaf6684ffe
Jarod007 <jarod.hamsa@gmail.com>
2014-12-10 09:29:13 -0600

    Started making the module menu accordian on the home page

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var $j = jQuery.noConflict();
+// $j is now an alias to the jQuery function; creating the new alias is optional.
+ 
+$j(document).ready(function() {
+    $j( "div" ).hide();
+});
+ 
+// The $ variable now has the prototype meaning, which is a shortcut for
+// document.getElementById(). mainDiv below is a DOM element, not a jQuery object.
+window.onload = function() {
+    var mainDiv = $( "main" );
+}
+
+$(document).ready(function() {
+    $('#module-menu').accordion();
+});
