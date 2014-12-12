a3431e5f1691f6b726bbbbf0efaa49a98bec8d89
dbradley771 <dbradley771@yahoo.com>
2014-12-12 09:33:04 -0600

    Disallowed website to scale under a 300px width.

--- javascript.js
+++ javascript.js
@@ -6,11 +6,24 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 	$('#circulating-planet').circulate({
-		speed: 1500,                  // Speed of each quarter segment of animation, 1000 = 1 second
-	    height: 100,                 // Distance vertically to travel
-	    width: 1350,                  // Distance horizontally to travel
-	    sizeAdjustment: 160,         // Percentage to grow or shrink
-	    loop: true,                 // Circulate continuously
-	    zIndexValues: [-1, 1, 1, -1]   // Sets z-index value at each stop of animation
+		speed: 1500,					// Speed of each quarter segment of animation, 1000 = 1 second
+	    height: 100,					// Distance vertically to travel
+	    width: 1350,					// Distance horizontally to travel
+	    sizeAdjustment: 160,			// Percentage to grow or shrink
+	    loop: true,						// Circulate continuously
+	    zIndexValues: [-1, 1, 1, -1]	// Sets z-index value at each stop of animation
 	});
 });
+
+function resize() {
+		var width = $(window).width();
+		if (width < 300) {
+				document.getElementById("navbar").className = document.getElementById("navbar").className.replace( /(?:^|\s)navbar-fixed-top(?!\S)/g , '' );
+		} else {
+			if (!document.getElementById("navbar").className.match(/(?:^|\s)navbar-fixed-top(?!\S)/)) {
+				document.getElementById("navbar").className += " navbar-fixed-top";
+			}
+		}
+};
+
+window.onresize = resize;
