d965f0b76646aad48b6a8dd197954374fda58ddc
Jarod007 <jarod.hamsa@gmail.com>
2014-12-15 09:44:55 -0600

    Finished circulate function, created new parallax background, and started working on module pages.

--- javascript.js
+++ javascript.js
@@ -8,9 +8,26 @@ $(document).ready(function(){
 	$('#circulating-planet').circulate({
 		speed: 1500,                  // Speed of each quarter segment of animation, 1000 = 1 second
 	    height: 100,                 // Distance vertically to travel
-	    width: 1350,                  // Distance horizontally to travel
+	    width: 1325,                  // Distance horizontally to travel
+	    sizeAdjustment: 160,         // Percentage to grow or shrink
+	    loop: true,                 // Circulate continuously
+	    zIndexValues: [-1, 1, 1, -1]   // Sets z-index value at each stop of animation
+	});
+	$('#circulating-planet-sm').circulate({
+		speed: 1500,                  // Speed of each quarter segment of animation, 1000 = 1 second
+	    height: 100,                 // Distance vertically to travel
+	    width: 400,                  // Distance horizontally to travel
 	    sizeAdjustment: 160,         // Percentage to grow or shrink
 	    loop: true,                 // Circulate continuously
 	    zIndexValues: [-1, 1, 1, -1]   // Sets z-index value at each stop of animation
 	});
 });
+
+$(window).scroll(function(e){
+	  parallax();
+	});
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.background').css('top',-(scrolled*.35)+'px');
+}
