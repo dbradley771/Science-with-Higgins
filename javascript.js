5999974c98c9f91efb3b11894d15b9ac51d69fff
dbradley771 <dbradley771@yahoo.com>
2014-12-16 09:03:21 -0600

    Testing if jQuery .noConflict() is on

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#module-menu').accordion({ clearStyle: true, autoHeight: false, collapsible: true}); 
+jQuery(document).ready(function() {
+	jQuery('#module-menu').accordion({ clearStyle: true, autoHeight: false, collapsible: true}); 
     //clearStyle: true, autoHeight: false makes the space of the accordion drop-down space equal to the 
     //height of only its paragraphs instead of the height of the largest accordion drop-down
 });
