01b75ce245a86be446b4637e79e40728def977b5
dbradley771 <dbradley771@yahoo.com>
2014-12-16 08:56:24 -0600

    Fix?

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	jQuery('#module-menu').accordion({ clearStyle: true, autoHeight: false, collapsible: true}); 
+    $('#module-menu').accordion({ clearStyle: true, autoHeight: false, collapsible: true}); 
     //clearStyle: true, autoHeight: false makes the space of the accordion drop-down space equal to the 
     //height of only its paragraphs instead of the height of the largest accordion drop-down
 });
 
 $(document).ready(function(){
 	$('#circulating-planet').circulate({
-		speed: 1500,					// Speed of each quarter segment of animation, 1000 = 1 second
-	    height: 100,					// Distance vertically to travel
-	    width: 1350,					// Distance horizontally to travel
-	    sizeAdjustment: 160,			// Percentage to grow or shrink
-	    loop: true,						// Circulate continuously
-	    zIndexValues: [-1, 1, 1, -1]	// Sets z-index value at each stop of animation
+		speed: 1500,                  // Speed of each quarter segment of animation, 1000 = 1 second
+	    height: 100,                 // Distance vertically to travel
+	    width: 1325,                  // Distance horizontally to travel
+	    sizeAdjustment: 160,         // Percentage to grow or shrink
+	    loop: true,                 // Circulate continuously
+	    zIndexValues: [-1, 1, 1, -1]   // Sets z-index value at each stop of animation
 	});
 	$('#circulating-planet-sm').circulate({
 		speed: 1500,                  // Speed of each quarter segment of animation, 1000 = 1 second
@@ -22,6 +22,24 @@ $(document).ready(function(){
 	    zIndexValues: [-1, 1, 1, -1]   // Sets z-index value at each stop of animation
 	});
 });
+
+$(window).scroll(function(e){
+	  parallax();
+	});
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.background').css('top',-(scrolled*.35)+'px');
+}
+
+$(window).scroll(function(e){
+	  parallax();
+	});
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.background').css('top',-(scrolled*.15)+'px');
+}
 
 function resize() {
 	var width = $(window).width();
@@ -35,12 +53,3 @@ function resize() {
 };
 
 window.onresize = resize;
-
-$(window).scroll(function(e){
-	  parallax();
-	});
-
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.background').css('top',-(scrolled*.15)+'px');
-}
